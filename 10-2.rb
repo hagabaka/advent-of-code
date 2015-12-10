44c5cdeee8b357f008295a3dfc5471b5f4298095
Yaohan Chen <ychen@agileoasis.com>
2015-12-10 13:06:07 -0500

    Add 10

---
+++ 10-2.rb
@@ -0,0 +1,9 @@
+input = '3113322113'
+
+output = nil
+50.times do |i|
+  input.gsub!(/1+|2+|3+/) do |match|
+    "#{match.length}#{match[0]}"
+  end
+  puts format('%2i %i', i + 1, input.length)
+end
