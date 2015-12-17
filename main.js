a24ada2f3f6a6dcd9a87a5a2f936365c229bc3af
Yaohan Chen <yaohan.chen@gmail.com>
2015-12-17 00:30:32 -0500

    Reorder code for the charts

--- main.js
+++ main.js
@@ -1,5 +1,59 @@
 requirejs(['config.js'], function() {
   require(['makeChart', 'domready!'], function(makeChart) {
+    require(['fetchLanguageData'], function(fetchLanguageData) {
+      var anchorText = 'GitHub search';
+      makeChart({
+        chart: {
+          renderTo: 'language-chart',
+          type: 'pie'
+        },
+        title: {text: 'Used Languages'},
+        subtitle: {
+          text: 'Based on ' + anchorText + ' of "advent" repositories owned by users listed on the leaderboard',
+          useHTML: true
+        },
+      }, function(languageChart) {
+        return fetchLanguageData.then(function(data) {
+          var subtitleOption = languageChart.options.subtitle;
+          subtitleOption.text = subtitleOption.text.replace(
+            anchorText,
+            '<a href="' + data.sourceUrl + '">' + anchorText + '</a>'
+          );
+          languageChart.setTitle(null, subtitleOption);
+          languageChart.addSeries({
+            name: data.label,
+            data: data.data,
+            dataLabels: {useHTML: true}
+          });
+        });
+      });
+    });
+
+    require(['fetchDomainData'], function(fetchDomainData) {
+      makeChart({
+        chart: {
+          renderTo: 'domain-chart',
+          type: 'pie'
+        },
+        title: {text: 'Profile Websites'},
+        subtitle: {text: 'Numbers of leaderboard entries whose profile links are on each domain'},
+        legend: {enabled: true}
+      }, function(domainChart) {
+        return fetchDomainData.then(function(data) {
+          data.forEach(function(day, index) {
+            var centerX = (index * 2 + 1) / (data.length * 2) * 100 + '%';
+            var centerY = '50%';
+            domainChart.addSeries({
+              name: day.date,
+              data: day.data,
+              dataLabels: {useHTML: true},
+              center: [centerX, centerY]
+            });
+          });
+        });
+      });
+    });
+
     require(['fetchTimeData'], function(fetchTimeData) {
       makeChart({
         chart: {
@@ -42,60 +96,6 @@ requirejs(['config.js'], function() {
           });
           timeChart.get('xAxis').setCategories(data[0].labels);
           timeChart.hideLoading();
-        });
-      });
-    });
-
-    require(['fetchDomainData'], function(fetchDomainData) {
-      makeChart({
-        chart: {
-          renderTo: 'domain-chart',
-          type: 'pie'
-        },
-        title: {text: 'Profile Websites'},
-        subtitle: {text: 'Numbers of leaderboard entries whose profile links are on each domain'},
-        legend: {enabled: true}
-      }, function(domainChart) {
-        return fetchDomainData.then(function(data) {
-          data.forEach(function(day, index) {
-            var centerX = (index * 2 + 1) / (data.length * 2) * 100 + '%';
-            var centerY = '50%';
-            domainChart.addSeries({
-              name: day.date,
-              data: day.data,
-              dataLabels: {useHTML: true},
-              center: [centerX, centerY]
-            });
-          });
-        });
-      });
-    });
-
-    require(['fetchLanguageData'], function(fetchLanguageData) {
-      var anchorText = 'GitHub search';
-      makeChart({
-        chart: {
-          renderTo: 'language-chart',
-          type: 'pie'
-        },
-        title: {text: 'Used Languages'},
-        subtitle: {
-          text: 'Based on ' + anchorText + ' of "advent" repositories owned by users listed on the leaderboard',
-          useHTML: true
-        },
-      }, function(languageChart) {
-        return fetchLanguageData.then(function(data) {
-          var subtitleOption = languageChart.options.subtitle;
-          subtitleOption.text = subtitleOption.text.replace(
-            anchorText,
-            '<a href="' + data.sourceUrl + '">' + anchorText + '</a>'
-          );
-          languageChart.setTitle(null, subtitleOption);
-          languageChart.addSeries({
-            name: data.label,
-            data: data.data,
-            dataLabels: {useHTML: true}
-          });
         });
       });
     });
