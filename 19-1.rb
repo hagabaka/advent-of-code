5450e094397d21449941aa9808dad6432b354ab5
Yaohan Chen <yaohan.chen@gmail.com>
2015-12-19 05:15:53 -0500

    Add 19

---
+++ 19-1.rb
@@ -0,0 +1,72 @@
+replacements = <<END.scan(/^(.+) => (.+)$/).map {|substring, new_substring| [Regexp.new(substring), new_substring]}
+Al => ThF
+Al => ThRnFAr
+B => BCa
+B => TiB
+B => TiRnFAr
+Ca => CaCa
+Ca => PB
+Ca => PRnFAr
+Ca => SiRnFYFAr
+Ca => SiRnMgAr
+Ca => SiTh
+F => CaF
+F => PMg
+F => SiAl
+H => CRnAlAr
+H => CRnFYFYFAr
+H => CRnFYMgAr
+H => CRnMgYFAr
+H => HCa
+H => NRnFYFAr
+H => NRnMgAr
+H => NTh
+H => OB
+H => ORnFAr
+Mg => BF
+Mg => TiMg
+N => CRnFAr
+N => HSi
+O => CRnFYFAr
+O => CRnMgAr
+O => HP
+O => NRnFAr
+O => OTi
+P => CaP
+P => PTi
+P => SiRnFAr
+Si => CaSi
+Th => ThCa
+Ti => BP
+Ti => TiTi
+e => HF
+e => NAl
+e => OMg
+END
+
+string = DATA.read
+require 'set'
+new_strings = Set.new
+replacements.each_with_index do |(pattern, new_substring), i|
+  replaced = ''
+  remaining = string
+  string.length.times do |n|
+    printf("Rule: %3i  Replacements: %3i  Unique Results: %10i\r", i, n, new_strings.length)
+   
+    new_string = replaced + remaining.sub(pattern) do
+      replaced = replaced + $` + $&
+      remaining = $'
+      new_substring
+    end
+
+    if new_string != string
+      new_strings.add new_string
+    else
+      break
+    end
+  end
+end
+puts "#{new_strings.length}"
+
+__END__
+CRnCaSiRnBSiRnFArTiBPTiTiBFArPBCaSiThSiRnTiBPBPMgArCaSiRnTiMgArCaSiThCaSiRnFArRnSiRnFArTiTiBFArCaCaSiRnSiThCaCaSiRnMgArFYSiRnFYCaFArSiThCaSiThPBPTiMgArCaPRnSiAlArPBCaCaSiRnFYSiThCaRnFArArCaCaSiRnPBSiRnFArMgYCaCaCaCaSiThCaCaSiAlArCaCaSiRnPBSiAlArBCaCaCaCaSiThCaPBSiThPBPBCaSiRnFYFArSiThCaSiRnFArBCaCaSiRnFYFArSiThCaPBSiThCaSiRnPMgArRnFArPTiBCaPRnFArCaCaCaCaSiRnCaCaSiRnFYFArFArBCaSiThFArThSiThSiRnTiRnPMgArFArCaSiThCaPBCaSiRnBFArCaCaPRnCaCaPMgArSiRnFYFArCaSiThRnPBPMgA
